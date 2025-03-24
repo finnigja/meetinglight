4d0d5de8cfaf656fb79763e155c38229e2f52ce4
Jamie Finnigan <jfinnigan@hashicorp.com>
2025-03-24 14:55:10 -0700

    tweak connectivity check timing

--- main.go
+++ main.go
@@ -246,8 +246,10 @@ func checkDeviceConnectivity() error {
 	fabric := createBasicFabric()
 	ch, err := connectDevice(fabric)
 	if err != nil {
+		time.Sleep(2 * time.Second)
 		ch, err = connectDevice(fabric)
 		if err != nil {
+			time.Sleep(2 * time.Second)
 			ch, err = connectDevice(fabric)
 			if err != nil {
 				return fmt.Errorf("failed to connect: %w", err)
